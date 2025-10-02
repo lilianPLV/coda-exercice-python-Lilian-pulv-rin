f864da52374ad86698c48bbc141b247662a29c5b
LilianPLV <lilianpulv@gmail.com>
2025-10-02 13:53:46 +0200

    exercice 14

--- main.py
+++ main.py
@@ -55,8 +55,10 @@ def exercice13():
     for i in range(5):
         print(i+1) 
 
-
-
+def exercice14():
+    for i in range(1,6):
+        print("2 x",i,"=",i*2)
+ 
 
 
 
@@ -92,6 +94,8 @@ def main():
         exercice12()
     elif choix == "13":
         exercice13()
+    elif choix == "14":
+        exercice14()
     else: 
         print("Exercice non reconnu.") 
 if __name__ == "__main__": 
